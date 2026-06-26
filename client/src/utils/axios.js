f415df7940f9ae9bfda3faeae49d03230ca4faf6
Saksham <guptasaksham795@gmail.com>
2026-06-26 14:09:00 +0530

    updated axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,15 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
+
+const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"
+    : "https://eventora-eventbookingplatform-server.onrender.com/api";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
